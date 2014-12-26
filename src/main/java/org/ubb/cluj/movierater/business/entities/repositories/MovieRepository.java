ce7628b10bc69c3b26d3d17fb57fca46f866c5be
Alex Somai <alex_somai@yahoo.com>
2014-12-26 10:41:30 +0200

    Displaying 9 items per page. Added thumbnail fragment.

--- src/main/java/org/ubb/cluj/movierater/business/entities/repositories/MovieRepository.java
+++ src/main/java/org/ubb/cluj/movierater/business/entities/repositories/MovieRepository.java
@@ -22,7 +22,7 @@ import java.util.Set;
 @Transactional(readOnly = true)
 public class MovieRepository {
 
-    public static final int MAX_ITEMS_PER_PAGE = 3;
+    public static final int MAX_ITEMS_PER_PAGE = 9;
     private static final String SORTING_ASC = "asc";
     private static final String SORTING_DESC = "desc";
     private static final List<String> AVAILABLE_SORTERS = Arrays.asList("rate", "numberOfRatings");
